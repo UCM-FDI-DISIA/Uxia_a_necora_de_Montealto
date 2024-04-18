ba2393231e7790fb0ee438861ddfd1a1ac3a5ab0
MiguelCGa <micurros@ucm.es>
2024-04-19 01:17:05 +0200

    Setup proyecto 2

---
+++ src/ComponentRegisterer.cpp
@@ -0,0 +1,10 @@
+#include "Factory.h"
+#include "ForgeImport.h"
+
+FORGE_IMPORT void registerComponents(Factory& f) {
+	/**
+        * Para registrar componentes dentro del motor
+        * hacer la siguiente llamada:
+        * f.registerComponent<ComponentName>();
+        */
+}
