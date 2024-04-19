4d07a1c7c6ac3916b7ee5411bc67a0c4e0b123ba
algonz39 <algonz39@ucm.es>
2024-04-19 10:47:44 +0200

    Kelp Component

---
+++ src/KelpComponent.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef KELP_COMPONENT_H_
+#define KELP_COMPONENT_H_
+#include "Component.h"
+#include <string>
+
+class Collider;
+class GameManager;
+
+class KelpComponent : public Component {
+private:
+	Collider* collider;
+	GameManager* gameManager;
+    int score;
+public:
+	static const std::string id;
+
+	KelpComponent();
+	bool initComponent(ComponentData* data) override;
+};
+
+#endif // !KELP_COMPONENT_H_
