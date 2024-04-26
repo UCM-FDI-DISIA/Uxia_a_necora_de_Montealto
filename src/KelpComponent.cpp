9368eda3ab2a68b2fd9534309c3069e8bbcbd0a9
algonz39 <algonz39@ucm.es>
2024-04-26 10:26:32 +0200

    Eliminado GameManager no necesario

--- src/KelpComponent.cpp
+++ src/KelpComponent.cpp
@@ -9,22 +9,20 @@ const std::string KelpComponent::id = "KelpComponent";
 
 KelpComponent::KelpComponent() :
 	collider(nullptr),
-    gameManager(nullptr),
     score(1){
     serializer(score, "score");
 }
 
 bool KelpComponent::initComponent(ComponentData* data) {
- //   Entity* manager = scene->getEntityByHandler("GameManager");
-	//if (/*manager->hasComponent<GameManager>() && */ entity->hasComponent<Collider>()) {
- //       //gameManager = manager->getComponent<GameManager>();
- //       collider = entity->getComponent<Collider>();
- //       collider->registerCallback([] (Collider* self, Collider* other){
- //           //TODO cuando se arreglen los callbacks
- //       });
+    Entity* manager = scene->getEntityByHandler("GameManager");
+	if (entity->hasComponent<Collider>()) {
+        collider = entity->getComponent<Collider>();
+        collider->registerCallback([] (Collider* self, Collider* other){
+            //TODO cuando se arreglen los callbacks
+        });
         return true;
- //   }
-	//return false;
+    }
+    return false;
 }
 
 
